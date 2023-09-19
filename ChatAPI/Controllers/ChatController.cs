3e33c0ae711551dccdae8503c2e102c8f52b1ff9
zakaria <zelkarmoudi@marketpoke.com>
2023-09-19 19:23:37 +0100

    api

---
+++ ChatAPI/Controllers/ChatController.cs
@@ -0,0 +1,48 @@
+﻿using ChatAPI.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using PusherServer;
+using System.Net;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ChatAPI.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ChatController : Controller
+    {
+        [HttpPost("messages")]
+        public async Task<ActionResult> Message(MessageDto dto )
+        {
+            var options = new PusherOptions
+            {
+                Cluster = "eu",
+                Encrypted = true
+            };
+
+            var pusher = new Pusher(
+       "1673010",
+       "2b33e5b65cdbdcfca5b9",
+       "14a39cbbc9080960246c",
+       options);
+
+            await pusher.TriggerAsync(
+              "chat",
+              "message",
+              new { 
+                  
+                  sender=dto.sender,
+                  receiver=dto.receiver,
+                  message = dto.message
+              
+              }
+              );
+
+            return Ok(new string[] { "message envoyée" });
+        
+        }
+
+       
+
+    }
+}
---
+++ ChatAPI/Dtos/MessageDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatAPI.Dtos
+{
+    public class MessageDto
+    {
+     
+        public string message { get; set; }
+        public string sender { get; set; }
+        public string receiver { get; set; }
+    }
+}
